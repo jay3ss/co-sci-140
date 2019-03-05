e77c0af6a69fa074a1e020adb34de405624bfda5
Jay Ess <mentoce@gmail.com>
2019-03-04 19:47:44 -0800

    Start work on programming quiz for assignment 3

---
+++ assignments/assignment03/assignment03.cpp
@@ -0,0 +1,19 @@
+// Joshua Saunders
+// CO SCI 140 (Section 15029) - Assignment 03
+// Due March 10, 2019
+//
+// Write a program that scores the following data about a soccer player in a
+// structure:
+//
+//     Player’s Name
+//     Player’s Number
+//     Points Scored by Player
+//
+// The program should keep an array of 12 of these structures. Each element is
+// for a different player on a team. When the program runs, it should ask the
+// user to enter the data for each player. It should then show a table that
+// lists each player’s number, name, and points scored. The program should also
+// calculate and display the total points earned by the team. The number and
+// name of the player who has earned the most points should also be displayed.
+//
+// Input Validation: Do no accept negative values for players’ numbers or points scored.
