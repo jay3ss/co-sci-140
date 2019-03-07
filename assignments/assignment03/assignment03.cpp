a94df1984880bc2d74cb5291b0aad6b624a3fd99
Jay Ess <mentoce@gmail.com>
2019-03-06 17:17:42 -0800

    Add Player struct, function to set info for Player struct, & start of main loop for assignment

--- assignments/assignment03/assignment03.cpp
+++ assignments/assignment03/assignment03.cpp
@@ -16,4 +16,55 @@
 // calculate and display the total points earned by the team. The number and
 // name of the player who has earned the most points should also be displayed.
 //
-// Input Validation: Do no accept negative values for players’ numbers or points scored.
+// Input Validation: Do no accept negative values for players’ numbers or
+// points scored.
+// 
+// NOTE: Extra credit has been attempted with making sure that no two players
+// have the same number.
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Struct that defines a soccer player:
+// - name (string):             name of the player
+// - number (integer):          player's number (no negative values)
+// - pointsScored (integer):    number of points the player scored during a
+//                              game (no negative values)
+struct Player
+{
+    string name;
+    int number;
+    int pointsScored;
+};
+
+// Sets the players information (i.e., name, number, and number of points)
+Player setPlayerInfo(Player *);
+
+int main()
+{
+    const int NUM_PLAYERS = 12;
+    Player players[NUM_PLAYERS];
+
+    // Set the information for each player in the players array
+    for (int i = 0; i < NUM_PLAYERS; i++)
+    {
+        cout << "PLAYER #" << i + 1 << "\n---------";
+        setPlayerInfo(&players[i]);
+        cout << "\n";
+    }
+
+    return 0;
+}
+
+Player setPlayerInfo(Player *player)
+{
+    cout << "\nPlayer name: ";
+    getline(cin, player->name);
+    cout << "Player's number: ";
+    cin >> player->number;
+    cin.ignore();
+    cout << "Points scored: ";
+    cin >> player->pointsScored;
+    cin.ignore();
+}
