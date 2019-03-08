4fb5de169e77585594c8331a49f964d4f4bf518c
Jay Ess <mentoce@gmail.com>
2019-03-08 14:27:04 -0800

    Update assignment 3 and test program

--- assignments/assignment03/assignment03.cpp
+++ assignments/assignment03/assignment03.cpp
@@ -27,21 +27,50 @@
 using namespace std;
 
 // Struct that defines a soccer player:
-// - name (string):             name of the player
-// - number (integer):          player's number (no negative values)
-// - pointsScored (integer):    number of points the player scored during a
-//                              game (no negative values)
+// - name (string):                  name of the player
+// - number (unsigned int):          player's number
+// - pointsScored (unsigned int):    number of points the player scored during
+//                                   a game
 struct Player
 {
     string name;
-    int number;
-    int pointsScored;
+    unsigned int number;
+    unsigned int pointsScored;
 };
 
-// Sets an individual player's information (i.e., name, number, and number of
-// points)
-void setPlayerInfo(Player *);
+/*
+ *  FUNCTION PROTOTYPES
+ */
+// Function that sets an individual player's information (i.e., name, number,
+// and number of points)
+void setPlayersInfo(Player [], const int);
 
+// Function that gets the player's number. No two players can have the same
+// number and the number must not be negative
+unsigned int getPlayerNumber(Player [], const int, const int);
+
+// Function that checks if a desired number (number) is currently taken by any
+// other players.
+bool isNumberTaken(const Player [], const int, const int, const int);
+
+// Displays each player's stats on an individual line, the total points scored
+// in the game, and displays the player with the most points
+void finalOutput(const Player players[], const int size);
+
+// Function that finds the player that scored the most points in the players
+// array and returns that player's index
+int highestScorerIndex(const Player players[], const int size);
+
+// Function that finds the total points scored in the game
+unsigned int totalPoints(const Player players[], const int size);
+
+// Convenience function that determines if an integer is negative
+bool isNegative(int);
+
+// Convenience function that gets an unsigned int from the user. Does
+// input validation and prints a message to the screen if the user
+// doesn't enter a non-negative number
+unsigned int getUnsignedInt();
 
 int main()
 {
@@ -52,21 +81,139 @@ int main()
     for (int i = 0; i < NUM_PLAYERS; i++)
     {
         cout << "PLAYER #" << i + 1 << "\n---------";
-        setPlayerInfo(&players[i]);
+        cout << "\nPlayer name: ";
+        getline(cin, players[i].name);
+        cout << "Player's number: ";
+        players[i].number = getPlayerNumber(players, i, NUM_PLAYERS);
+        cout << "Points scored: ";
+        players[i].pointsScored = getUnsignedInt();
         cout << "\n";
     }
+
+    finalOutput(players, NUM_PLAYERS);
 
     return 0;
 }
 
-void setPlayerInfo(Player *player)
+// void setPlayersInfo(Player players[], const int size)
+// {
+//     for (int i = 0; i < size; i++)
+//     {
+//         cout << "PLAYER #" << i + 1 << "\n---------";
+//         cout << "\nPlayer name: ";
+//         getline(cin, players[i]->name);
+//         cout << "Player's number: ";
+//         players[i]->number = getUnsignedInt();
+//         cout << "Points scored: ";
+//         players[i]->pointsScored = getUnsignedInt();
+//         cout << "\n";
+//     }
+// }
+
+// Function that gets the player's number. No two players can have the same
+// number and the number must not be negative.
+// HACK: For now, acts as a shim to the getUnsignedInt function
+unsigned int getPlayerNumber(Player players[], const int index, const int size)
 {
-    cout << "\nPlayer name: ";
-    getline(cin, player->name);
-    cout << "Player's number: ";
-    cin >> player->number;
-    cin.ignore();
-    cout << "Points scored: ";
-    cin >> player->pointsScored;
-    cin.ignore();
+    int number = -1;
+    bool isTaken = true;
+
+    // Keep trying to get the player number until the number is both
+    // non-negative AND the number is not taken.
+    while (isNegative(number) || isTaken)
+    {
+        number = getUnsignedInt();
+        isTaken = isNumberTaken(players, number, index, size);
+
+        if (isTaken) {
+            cout << number << " is taken. Please try again.\n";
+        }
+    }
+
+    return number;
+}
+
+// Function that checks if a desired number (number) is currently taken by any
+// other players.
+bool isNumberTaken(const Player players[], const int number,
+                   const int index, const int size)
+{
+    int stop;
+
+    // Make sure that we're not going beyond the bounds of the array
+    if (index < size)
+    {
+        stop = index;
+    }
+    else
+    {
+        stop = size;
+    }
+
+    for (int i = 0; i < stop; i++)
+    {
+        if (players[i].number == number)
+        {
+            return true;
+        }
+    }
+
+    return players[index].number == number;
+}
+
+// Displays each player's stats on an individual line, the total points scored
+// in the game, and displays the player with the most points
+void finalOutput(const Player players[], const int size)
+{
+    unsigned int total = totalPoints(players, size);
+    int highestIndex = highestScorerIndex(players, size);
+
+    cout << "NAME\t\tNUMBER\t\tPOINTS SCORED\n";
+    for (int i = 0; i < size; i++)
+    {
+        cout << players[i].name << "\t\t "
+             << players[i].number << "\t\t "
+             << players[i].pointsScored << "\n";
+    }
+
+    cout << "TOTAL POINTS: " << total << "\n"
+         << "The player who scored the most points is: "
+         << players[highestIndex].name;
+}
+
+// Function that finds the player that scored the most points in the players
+// array and returns that player's index
+int highestScorerIndex(const Player players[], const int size)
+{
+    return 0;
+}
+
+// Function that finds the total points scored in the game
+unsigned int totalPoints(const Player players[], const int size)
+{
+    return 0;
+}
+
+// Convenience function that gets an unsigned int from the user. Does
+// input validation and prints a message to the screen if the user
+// doesn't enter a non-negative number
+unsigned int getUnsignedInt()
+{
+    int number = -1;
+    do {
+        cin >> number;
+        cin.ignore();
+        if (isNegative(number))
+        {
+            cout << "No negative numbers please\n";
+        }
+    } while(isNegative(number));
+
+    return number;
+}
+
+// Convenience function that determines if an integer is negative
+bool isNegative(int number)
+{
+    return number < 0;
 }
