a51927315c839c41ed2db2e6b542bfe99300b7b4
Jay Ess <mentoce@gmail.com>
2019-04-28 14:43:58 -0700

    Add javadocs to implementation file

--- assignments/assignment06/Circle.cpp
+++ assignments/assignment06/Circle.cpp
@@ -1,5 +1,9 @@
 #include "Circle.h"
 
+/** Calculates the area of the circle using the formula
+ area = 3.14159 * radius * radius
+ and sets the area data member to this value using the inherited setArea
+ function. */
 void Circle::calcArea()
 {
     setArea(3.14159 * radius * radius);
--- assignments/assignment06/Rectangle.cpp
+++ assignments/assignment06/Rectangle.cpp
@@ -1,5 +1,9 @@
 #include "Rectangle.h"
 
+/** Calculates the area of the rectangle using the formula
+ area = length * width
+ and sets the area data member to this value using the inherited setArea
+ function. */
 void Rectangle::calcArea()
 {
     setArea(length * width);
