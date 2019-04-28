ddbd5bf52ef07dde365e98a40eaec78d549c8c19
Jay Ess <mentoce@gmail.com>
2019-04-28 13:06:53 -0700

    Update definition of Rectangle class

--- assignments/assignment06/Rectangle.h
+++ assignments/assignment06/Rectangle.h
@@ -6,6 +6,13 @@
 
 class Rectangle : public BasicShape
 {
+private:
+    long int width;
+    long int length;
+public:
+    Rectangle(const long int w = 0, const long int l = 0) : width(w), length(l)
+    { calcArea(); }
+    void calcArea();
 };
 
 #endif // _RECTANGLE_H
