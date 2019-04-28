d46dbcaa8188d0fddad8848571c3805872b9e62f
Jay Ess <mentoce@gmail.com>
2019-04-28 13:47:24 -0700

    Finish declaration of Circle class

--- assignments/assignment06/Circle.h
+++ assignments/assignment06/Circle.h
@@ -6,7 +6,23 @@
 
 class Circle : public BasicShape
 {
-
+private:
+    long int centerX;
+    long int centerY;
+    double radius;
+public:
+    Circle(const long int cx = 0, const long int cy = 0, const double r = 0) :
+        centerX(cx), centerY(cy), radius(r)
+        { calcArea(); }
+    void setCenterX(const long int cx)
+        { centerX = cx; }
+    void setCenterY(const long int cy)
+        { centerY = cy; }
+    long int getCenterX() const
+        { return centerX; }
+    long int getCenterY() const
+        { return centerY; }
+    void calcArea();
 };
 
 #endif // _CIRCLE_H
