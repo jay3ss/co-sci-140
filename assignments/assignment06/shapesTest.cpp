f739203028778b8ce7e6c506a6b1401851ca6fb0
Jay Ess <mentoce@gmail.com>
2019-04-28 12:39:34 -0700

    Basic 'working' executable

---
+++ assignments/assignment06/shapesTest.cpp
@@ -0,0 +1,18 @@
+// Joshua Saunders
+// CO SCI 140 (Section 15029) - Chapter 15 Quiz
+// Due April 28, 2019
+// 
+// A driver program that defines a Circle object and a Rectangle object.
+// Demonstrate that each object properly calculates and reports its area.
+#include <iomanip>
+#include <iostream>
+#include "Circle.h"
+#include "Rectangle.h"
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
