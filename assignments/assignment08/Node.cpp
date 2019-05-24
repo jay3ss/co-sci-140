0b17d44a04faee3281e443eca2aff997e6091edc
Jay Ess <mentoce@gmail.com>
2019-05-23 17:15:21 -0700

    Implement getters and setters

--- assignments/assignment08/Node.cpp
+++ assignments/assignment08/Node.cpp
@@ -9,3 +9,25 @@ Node::Node(int num, Node* nextPtr)
 Node::~Node()
 {
 }
+
+// Getters
+int Node::data()
+{
+    return data_;
+}
+
+Node* Node::next()
+{
+    return next_;
+}
+
+// Setters
+void Node::data(int num)
+{
+    data_ = num;
+}
+
+void Node::next(Node* nextPtr)
+{
+    next_ = nextPtr;
+}
